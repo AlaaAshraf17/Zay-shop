2a4023c758d35686293d01dc5dbf75a587fb26a6
mahamahmoud-23 <mahamahmoud908@gmail.com>
2025-05-03 21:02:32 +0300

    save login/register changes

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,11 +1,47 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 const Login = () => {
   return (
     <div>
-      <h1>Login</h1>
-    </div>
-  )
-}
+      <h1>login</h1>
+      <div className="flex items-center justify-center min-h-screen bg-white text-black">
+      <div className="bg-gray-100 p-8 rounded-2xl shadow-lg w-full max-w-md border border-gray-300">
+      <h2 className="text-xl font-bold text-center text-green-600 mb-4">login</h2>
 
-export default Login
+        <form>
+          <div className="mb-4">
+            <label className="block mb-2 text-sm font-medium">email</label>
+            <input
+              type="email"
+              required
+              className="w-full p-3 rounded bg-white border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-500"
+              placeholder="example@email.com"
+            />
+          </div>
+
+          <div className="mb-6">
+            <label className="block mb-2 text-sm font-medium">password</label>
+            <input
+              type="password"
+              required
+              className="w-full p-3 rounded bg-white border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-500"
+              placeholder="********"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded transition"
+          >
+            <Link to="/"> login</Link>
+           
+          </button>
+          
+        </form>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 const Register = () => {
   return (
     <div>
+      
       <h1>Register</h1>
+      <div className="bg-white w-screen h-screen flex items-center justify-center p-0 m-0">
+      <div className="w-[320px] bg-white border border-gray-300 rounded-xl shadow-md p-6">
+        <h2 className="text-xl font-bold text-center text-green-600 mb-4">register</h2>
+
+        <form>
+          <div className="mb-3">
+            <label className="block text-sm text-gray-700 mb-1">name</label>
+            <input
+              type="text"
+              required
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
+              placeholder='name'
+            />
+          </div>
+
+          <div className="mb-3">
+            <label className="block text-sm text-gray-700 mb-1">email</label>
+            <input
+              type="email"
+              required
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
+              placeholder="example@email.com"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-sm text-gray-700 mb-1">password</label>
+            <input
+              type="password"
+              required
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
+              placeholder="********"
+            />
+          </div>
+
+          <button 
+            type="submit"
+            className="w-full bg-green-500 hover:bg-green-600 text-white font-semibold py-2 rounded-md"
+          >
+           <Link to="/">register</Link>
+            
+          </button>
+          <p className="mt-4 text-sm text-center text-gray-600">
+             you have an account
+            <Link to="/login" className="text-green-600 hover:underline ml-1">
+             login
+            </Link>
+          </p>
+        </form>
+      </div>
+    </div>
     </div>
   )
 }
